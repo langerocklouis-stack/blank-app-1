7b9cf01065fa4c2c3500020cd74789732de992e4
langerocklouis-stack <langerock.louis@gmail.com>
2026-04-08 10:18:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
+import pandas as pd
 
 st.title("🎈 César est gay")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+st.title("Graphique en barres")
+# Données exemple
+data = pd.DataFrame({
+    "Catégorie": ["A", "B", "C", "D", "E"],
+    "Valeur": [23, 45, 56, 78, 32]
+})
+# Graphique en barres
+st.bar_chart(data.set_index("Catégorie"))
